b805966ce9c43f52fcd1f47670bead44026651d8
DoVanDuyHedspi <vanduy07c.r@gmail.com>
2020-09-25 11:07:40 +0700

    week 1: homework

---
+++ src/component/body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/body.css';
+
+function Body() {
+  return (
+    <div className="body col-md-12 p-2">
+      <h2>Body</h2>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/component/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/header.css';
+
+function Header() {
+  return (
+    <div className="header col-md-12 p-2">
+      <h2>Header</h2>
+    </div>
+  );
+}
+
+export default Header;

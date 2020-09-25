3c259828ff38b9ff5b451461f7fb9ff63ec2c470
DoVanDuyHedspi <vanduy07c.r@gmail.com>
2020-09-25 09:45:36 +0700

    Updates

---
+++ precache-manifest.19e877623169c5da5acb1b85133bc4be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89d8260015b888fc46ddac00d7e4fca",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "8e2ab07aa0214ed9ab07",
+    "url": "/learn-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/learn-react/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn-react/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2ab07aa0214ed9ab07",
+    "url": "/learn-react/static/js/main.c11fb296.chunk.js"
+  },
+  {
+    "revision": "c99d4fac56664b7c4b54",
+    "url": "/learn-react/static/js/runtime-main.a82f666d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
